45e158458f849fae17fe0f01cfe86a960073fcf7
acintr <cintronalexj97@gmail.com>
2018-10-26 13:09:38 -0400

    minor change

--- something.py
+++ something.py
@@ -4,3 +4,5 @@ print("this is a  git tutorial I am doing")
 for i in range(0,10):
 	print(i)
 print('This is cool')
+x = input("Please enter the password to continue")
+print("There was never a password, ha!")
